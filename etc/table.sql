7933e30127ade41c5367e143205b4b4c35a6086b
michaelyin <gt8442b@yahoo.com>
2017-11-27 11:39:56 -0500

    create table sql script.

---
+++ etc/table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `equation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_t` datetime NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `latex` varchar(255) NOT NULL,
+  `verified` bit(1) DEFAULT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `modify_t` datetime DEFAULT NULL,
+  `version` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
